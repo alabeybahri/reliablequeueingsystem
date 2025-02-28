06fb65da78ea737592682e29d58b95a08bae5209
Bahri Alabey <alabeybb@gmail.com>
2025-02-28 18:59:22 +0100

    add broker multicasting logic

--- src/broker/Broker.java
+++ src/broker/Broker.java
@@ -1,22 +1,33 @@
 package broker;
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Broker {
     private int port;
     Map<String, List<Integer>> queues = new ConcurrentHashMap<>();
     public Map<String, Map<String, Integer>> clientOffsets = new ConcurrentHashMap<>(); // clientId -> (queueName -> offset)
-    private Set<String> currentClients = ConcurrentHashMap.newKeySet();
+    private MulticastSocket brokerMulticastSocket;
+    private InetSocketAddress brokerGroup;
+    private List<String> knownBrokers;
 
+    private static final String CLIENT_MULTICAST_ADDRESS = "224.0.0.1";
+    private static final int CLIENT_MULTICAST_PORT = 5010;
+    private static final String BROKER_MULTICAST_ADDRESS = "224.0.0.2";
+    private static final int BROKER_MULTICAST_PORT = 5020;
+    private static final int DISCOVERY_TIMEOUT = 1000; // 1 second timeout
 
-    private static final String MULTICAST_ADDRESS = "224.0.0.1";
-    private static final int MULTICAST_PORT = 5010;
-
-    public Broker(int port) {
+    public Broker(int port) throws IOException {
         this.port = port;
         startMulticastListener();
+        createBrokerMulticastSocket();
+        startBrokerMulticastListener();
+        startPeriodicPing();
     }
 
     public void start() {
@@ -25,9 +36,9 @@ public class Broker {
             while (true) {
                 Socket socket = serverSocket.accept();
                 String clientId = socket.getInetAddress().getHostAddress() + ":" + socket.getPort();
-                currentClients.add(clientId);
                 System.out.println("Client connected: " + clientId);
                 new Thread(new ClientHandler(socket, clientId, this)).start();
+
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -35,19 +46,48 @@ public class Broker {
     }
 
     public synchronized void removeClient(String clientId) {
-        currentClients.remove(clientId);
         clientOffsets.remove(clientId);
         System.out.println("Client disconnected: " + clientId);
+    }
+
+    private void startPeriodicPing() {
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        Random random = new Random();
+
+        scheduler.scheduleAtFixedRate(() -> {
+            try {
+                Thread.sleep(random.nextInt(10000)); // Random delay between 0-10s
+                sendPingRequest();
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+        }, 0, 10, TimeUnit.SECONDS);
+    }
+
+    private void sendPingRequest() throws IOException {
+        String message = "PING_BROKERS";
+        byte[] messageBytes = message.getBytes();
+        DatagramPacket packet = new DatagramPacket(messageBytes, messageBytes.length, brokerGroup.getAddress(), BROKER_MULTICAST_PORT);
+        brokerMulticastSocket.send(packet);
+        System.out.println("Sent ping request to " + BROKER_MULTICAST_ADDRESS + ":" + BROKER_MULTICAST_PORT);
+    }
+
+    private void sendPingResponse(InetAddress address, int port) throws IOException {
+        String response = String.valueOf(port);
+        byte[] responseBytes = response.getBytes();
+        DatagramPacket responsePacket = new DatagramPacket(responseBytes, responseBytes.length, address, port);
+        brokerMulticastSocket.send(responsePacket);
+        System.out.println("Responded to ping request with " + response);
     }
 
     private void startMulticastListener() {
         new Thread(() -> {
             try {
-                MulticastSocket multicastSocket = new MulticastSocket(MULTICAST_PORT);
-                InetAddress mcastaddr = InetAddress.getByName(MULTICAST_ADDRESS);
+                MulticastSocket multicastSocket = new MulticastSocket(CLIENT_MULTICAST_PORT);
+                InetAddress mcastaddr = InetAddress.getByName(CLIENT_MULTICAST_ADDRESS);
                 NetworkInterface netIf = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
-                multicastSocket.joinGroup(new InetSocketAddress(mcastaddr, MULTICAST_PORT), netIf);
-                System.out.println("Broker listening for multicast discovery on " + MULTICAST_ADDRESS + ":" + MULTICAST_PORT);
+                multicastSocket.joinGroup(new InetSocketAddress(mcastaddr, CLIENT_MULTICAST_PORT), netIf);
+                System.out.println("Broker listening for multicast discovery on " + CLIENT_MULTICAST_ADDRESS + ":" + CLIENT_MULTICAST_PORT);
 
 
                 while (true) {
@@ -71,6 +111,48 @@ public class Broker {
                 throw new RuntimeException(e);
             }
         }).start();
+    }
+
+    private void createBrokerMulticastSocket() throws IOException {
+        MulticastSocket multicastSocket = new MulticastSocket(BROKER_MULTICAST_PORT);
+        InetAddress mcastaddr = InetAddress.getByName(BROKER_MULTICAST_ADDRESS);
+        NetworkInterface netIf = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
+        InetSocketAddress group = new InetSocketAddress(mcastaddr, BROKER_MULTICAST_PORT);
+        multicastSocket.joinGroup(group, netIf);
+        multicastSocket.setOption(StandardSocketOptions.IP_MULTICAST_LOOP, false);
+        brokerGroup = group;
+        brokerMulticastSocket = multicastSocket;
+    }
+
+
+
+    private void startBrokerMulticastListener() {
+        new Thread(() -> {
+            try {
+                System.out.println("Broker listening for multicast ping on " + BROKER_MULTICAST_ADDRESS + ":" + BROKER_MULTICAST_PORT);
+                while (true) {
+                    byte[] buffer = new byte[256];
+                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                    brokerMulticastSocket.receive(packet);
+                    String message = new String(packet.getData(), 0, packet.getLength());
+
+                    if ("PING_BROKERS".equals(message)) {
+                        sendPingResponse(packet.getAddress(), packet.getPort());
+                    } else {
+                        registerBroker(packet);
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
+    private void registerBroker(DatagramPacket packet) {
+        String brokerInfo = packet.getAddress().getHostAddress() + ":" + new String(packet.getData(), 0, packet.getLength());
+        if (!brokerInfo.equals(port) && knownBrokers.add(brokerInfo)) {
+            System.out.println("Discovered broker: " + brokerInfo);
+        }
     }
 }
 
--- src/broker/BrokerApplication.java
+++ src/broker/BrokerApplication.java
@@ -1,7 +1,9 @@
 package broker;
 
+import java.io.IOException;
+
 public class BrokerApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int port = Integer.parseInt(args[0]);
         // Leader is the broker with the highest port (e.g., 5002)
         boolean isLeader = port == 5002;
