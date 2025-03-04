8973f2f5049ceba4e86fb30b582ff47dc06522f0
Furkan Bülbül <furknblbl@gmail.com>
2025-03-04 17:24:57 +0100

    Update Address.java

--- src/common/Address.java
+++ src/common/Address.java
@@ -34,6 +34,13 @@ public class Address implements Serializable {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        return host.equals(((Address) o).host) && port == ((Address) o).port;
+    }
+
+    @Override
     public int hashCode() {
         return Objects.hash(host, port);
     }
